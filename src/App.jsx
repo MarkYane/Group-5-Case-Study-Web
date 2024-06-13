1e98158280d65656845f78ef12d9b7cc6c675e77
MarkYane <markyane12@gmail.com>
2024-06-13 16:25:16 +0800

    Dashboard 90% done --Pending Ainez's final design

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react"
 import LoginPage from './Components/Login.jsx'
 import RegisterPage from './Components/Register.jsx'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-
+import Dashboard from './Components/Dashboard.jsx'
 const router = createBrowserRouter([
   {
       path:'/',
@@ -18,7 +18,9 @@ const router = createBrowserRouter([
 function App() {
   return (
     <>
-      <RouterProvider router={router}/>
+    <Dashboard></Dashboard>
+      {/* <RouterProvider router={router}/> */}
+      
     </>
   );
 }
